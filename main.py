8d7230b3eca13dc0cde6b535b7186a67222f5554
silnex <silnex@silnex.kro.kr>
2018-01-02 13:06:32 +0900

    Upload source code only

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import sys
 import socket
 # targetip = socket.gethostbyname(hostname)
-port = 22
 
-iplists = []
-ipsubfix = "192.168.0"
-for num in range(0,100):
-    iplists.append(ipsubfix + "." + str(num))
+def getIPlists(iprefix = "192.168.0"):
+    ''' 
+    '''
+    iplists = []
+    for num in range(0,255):
+        iplists.append(iprefix + "." + str(num))
+    return iplists
 
-ipcount = 0
-for targetip in iplists:
-    ipcount = ipcount + 1
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
-        result = sock.connect_ex((targetip, port))
-        if result == 0:
-            print("{}:{} is open".format(targetip, port))
-            sock.close()
-    except KeyboardInterrupt:
-        input("Press Enter to EXIT")
-        sys.exit()
-    # finally:
-    #     print("{}:{} is closed".format(targetip, port))
-input("Press Enter to EXIT")
+def main():
+    '''
+    iprefix = sys.argv[1]
+    port = sys.argv[2]
+    '''
+    iprefix = "192.168.0"
+    port = sys
+    iplists = getIPlists(iprefix)
+    for targetip in iplists:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(0.5)    # set time out 0.5sec
+            result = sock.connect_ex((targetip, port))
+            if result == 0:
+                print("{}:{} is open".format(targetip, port))
+                sock.close()
+        except KeyboardInterrupt:
+            input("Press Enter to EXIT")
+            sys.exit()
+    input("Press Enter to EXIT")
 
+if __name__ == "__main__":
+    main()
 
 # ========Todo========
 # 1. ping 구현        done
